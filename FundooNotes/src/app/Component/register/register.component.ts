4ee7692dd0eed4cd14db987b6509d70b687c50c7
ahamedsahib <adsahib39@gmail.com>
2021-09-15 17:13:43 +0530

    [Ahamed]Add:Validation of password and confirm password

--- FundooNotes/src/app/Component/register/register.component.ts
+++ FundooNotes/src/app/Component/register/register.component.ts
@@ -12,38 +12,37 @@ export class RegisterComponent implements OnInit {
 
   // variable - default false
   hide: boolean = false;
-  constructor() { 
-  }
+
+  isVisible:boolean= true;
+
+  constructor() { }
 
     ngOnInit(): void {
       this.RegisterForm = new FormGroup({
         firstName: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]{2,}'),Validators.minLength(3)]),
         lastName: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]{2,}'),Validators.minLength(3)]),
         email: new FormControl('',[Validators.required, Validators.email]),
-       password:new FormControl('',[Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$'),Validators.minLength(8)]),
-        confirmPassword:new FormControl('',[Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$'),Validators.minLength(8)])
-      })
+        password:new FormControl('',[Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$'),Validators.minLength(8)]),
+        confirmPassword:new FormControl('',[Validators.required])
+      });
     }
-    password() {
+    ShowPassword() {
       this.hide = !this.hide;
   }
 
-  getErrorMessage(inputName:string)
+  getErrorMessage(inputName:any)
    {
-    let len = inputName=="password"||inputName=="confirmPassword"? 8:3;
-      if (this.RegisterForm.controls[`${inputName}`].hasError('required')) {
-        return 'You must enter a value';
-      }
-      else if(this.RegisterForm.controls[`${inputName}`].hasError('minlength')){
-        return `minimum ${len} characters`;
-      }
-            else if(this.RegisterForm.controls[`${inputName}`].hasError('email'))
-      {
-        return `${inputName} is invalid`;
-      }
-      return this.RegisterForm.controls[`${inputName}`].hasError('pattern') ? `${inputName} is invalid` : '';
+      let len = inputName=="password"? 8:3;
+        if (this.RegisterForm.controls[`${inputName}`].hasError('required')) {
+          return 'You must enter a value';
+        }
+        else if(this.RegisterForm.controls[`${inputName}`].hasError('minlength')){
+          return `minimum ${len} characters`;
+        }
+              else if(this.RegisterForm.controls[`${inputName}`].hasError('email'))
+        {
+          return `${inputName} is invalid`;
+        }
+        return this.RegisterForm.controls[`${inputName}`].hasError('pattern') ? `${inputName} is invalid` : '';
+    }
 }
-
-}
-
-
