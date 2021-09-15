ef502b553806a7164a25a56d13fbbbf72a67bbfc
ahamedsahib <adsahib39@gmail.com>
2021-09-15 13:44:20 +0530

    [Ahamed]Add:validation to the fields

--- FundooNotes/src/app/Component/register/register.component.ts
+++ FundooNotes/src/app/Component/register/register.component.ts
@@ -12,17 +12,38 @@ export class RegisterComponent implements OnInit {
 
   // variable - default false
   hide: boolean = false;
-  constructor() { }
-
-  ngOnInit(): void {
-    this.RegisterForm = new FormGroup({
-      firstName: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]{2,}'),Validators.minLength(3)]),
-      lastName: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]{2,}'),Validators.minLength(3)])
-    })
+  constructor() { 
   }
-  password() {
-    this.hide = !this.hide;
+
+    ngOnInit(): void {
+      this.RegisterForm = new FormGroup({
+        firstName: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]{2,}'),Validators.minLength(3)]),
+        lastName: new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[a-zA-Z]{2,}'),Validators.minLength(3)]),
+        email: new FormControl('',[Validators.required, Validators.email]),
+       password:new FormControl('',[Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$'),Validators.minLength(8)]),
+        confirmPassword:new FormControl('',[Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$'),Validators.minLength(8)])
+      })
+    }
+    password() {
+      this.hide = !this.hide;
+  }
+
+  getErrorMessage(inputName:string)
+   {
+    let len = inputName=="password"||inputName=="confirmPassword"? 8:3;
+      if (this.RegisterForm.controls[`${inputName}`].hasError('required')) {
+        return 'You must enter a value';
+      }
+      else if(this.RegisterForm.controls[`${inputName}`].hasError('minlength')){
+        return `minimum ${len} characters`;
+      }
+            else if(this.RegisterForm.controls[`${inputName}`].hasError('email'))
+      {
+        return `${inputName} is invalid`;
+      }
+      return this.RegisterForm.controls[`${inputName}`].hasError('pattern') ? `${inputName} is invalid` : '';
 }
 
 }
+
 
